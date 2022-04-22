581a19e8494f031263a7b08d9ae2ba7cf3ca64f7
sevdot <sevdot8@gmail.com>
2022-04-22 22:05:59 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>sd-blog 博客应用</title>
+    <style>
+        .hero {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<section class="hero">
+    <div class="hero-body">
+        <h1 class="title">Sd-Blog</h1>
+        <p class="subtitle">SevDot 的个人博客</p>
+        <a href="https://github.com/sevdot" class="button">
+            <span class="icon">
+                <i class="fa fa-github"></i>
+            </span>
+            <span>Github</span>
+        </a>
+    </div>
+</section>
+</body>
+</html>
