cda530fc2fd765750026009f718af8740b7773c8
sevdot <sevdot8@gmail.com>
2022-04-24 14:26:58 +0800

    column migration

--- database/migrations/2022_04_24_033926_create_columns_table.php
+++ database/migrations/2022_04_24_033926_create_columns_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('columns', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('columns');
     }
 };
