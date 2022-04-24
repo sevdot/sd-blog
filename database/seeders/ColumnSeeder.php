60f77252e4b5694d544dadb97635e488d597540d
sevdot <sevdot8@gmail.com>
2022-04-24 17:10:14 +0800

    column seeder

--- database/seeders/ColumnSeeder.php
+++ database/seeders/ColumnSeeder.php
@@ -2,18 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\Column;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ColumnSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ColumnSeeder::class);
+        Column::factory()->count(10)->create();
     }
 }
