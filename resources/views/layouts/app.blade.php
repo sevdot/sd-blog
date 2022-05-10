8c938d6a9e1c9db58246e00eb0f53a510e1e45e7
sevdot <sevdot8@gmail.com>
2022-05-10 09:55:24 +0800

    baidu analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="shortcut icon" type="image/x-ico" sizes="16x16" href="/favicon.ico"/>
     <meta name="author" content="sevdot(sevdot8@gmail.com)"/>
     <meta name="keywords" content="SevDot，独立开发，自由职业"/>
-    <meta name="description" content="崇尚独立开发，追求自由职业，记录在独立自由上的探索。"/>
+    <meta name="description" content="@yield('description','崇尚独立开发，追求自由职业，记录在独立自由上的探索。')"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
@@ -18,5 +18,14 @@
 @include('layouts.header')
 @yield('content')
 @include('layouts.footer')
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?19fa46eca5bd50ab3344282fc4153512";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+</script>
 </body>
 </html>
